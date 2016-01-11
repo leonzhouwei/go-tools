28a8a56b577afa3defaed1219ae6e2f73bc5de16
Wei Zhou <zhouwei@qiniu.com>
2016-01-11 16:35:42 +0800

    chore: add the auto-download script

---
+++ download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+go get -u github.com/nsf/gocode
+go get -u github.com/jstemmer/gotags
+go get -u github.com/ChimeraCoder/gojson
+go get -u github.com/clipperhouse/gen
+go get -u github.com/ncw/gotemplate
+go get -u github.com/cihangir/gene
+go get -u github.com/drone/sqlgen
+go get -u github.com/tv42/becky
+go get -u github.com/alexandre-normand/safekeeper
+go get -u github.com/bslatkin/joiner
+go get -u github.com/akualab/optioner
+go get -u github.com/kisielk/errcheck
+go get -u github.com/opennota/check
+go get -u github.com/alecthomas/gometalinter
+go get -u github.com/dvyukov/go-fuzz
+go get -u github.com/bradfitz/goimports
+
+exit 0
+
