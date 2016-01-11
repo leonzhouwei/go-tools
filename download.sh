b228f39bd439b765f917bdd58a55e14661051fe8
Wei Zhou <zhouwei@qiniu.com>
2016-01-11 17:08:27 +0800

    chore: update

--- download.sh
+++ download.sh
@@ -1,21 +1,71 @@
 #!/bin/bash
 
+echo "----------------------------------------------------------------------"
 go get -u github.com/nsf/gocode
+echo "go get -u github.com/nsf/gocode done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/jstemmer/gotags
+echo "go get -u github.com/jstemmer/gotags done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/ChimeraCoder/gojson
+echo "go get -u github.com/ChimeraCoder/gojson done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/clipperhouse/gen
+echo "go get -u github.com/clipperhouse/gen done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/ncw/gotemplate
+echo "go get -u github.com/ncw/gotemplate done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/cihangir/gene
+echo "go get -u github.com/cihangir/gene done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/drone/sqlgen
+echo "go get -u github.com/drone/sqlgen done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/tv42/becky
+echo "go get -u github.com/tv42/becky done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/alexandre-normand/safekeeper
+echo "go get -u github.com/alexandre-normand/safekeeper done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/bslatkin/joiner
+echo "go get -u github.com/bslatkin/joiner done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/akualab/optioner
-go get -u github.com/kisielk/errcheck
+echo "go get -u github.com/akualab/optioner done"
+
+#echo "----------------------------------------------------------------------"
+#go get -u github.com/kisielk/errcheck
+#echo "go get -u github.com/kisielk/errcheck done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/opennota/check
-go get -u github.com/alecthomas/gometalinter
+echo "go get -u github.com/opennota/check done"
+
+#echo "----------------------------------------------------------------------"
+#go get -u github.com/alecthomas/gometalinter
+#echo "go get -u github.com/alecthomas/gometalinter done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/dvyukov/go-fuzz
+echo "go get -u github.com/dvyukov/go-fuzz done"
+
+echo "----------------------------------------------------------------------"
 go get -u github.com/bradfitz/goimports
+echo "go get -u github.com/bradfitz/goimports done"
+
+echo "----------------------------------------------------------------------"
+echo "all done"
 
 exit 0
 
